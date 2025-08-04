acb7fcdc4321d146781c2b5e8b8ac279fef5342d
Dinar Hadiyanto <dinarhadiyanto@Dinars-MacBook-Air.local>
2025-08-04 07:06:59 +0700

    chore: do nothing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> std::io::Result<()> {
+    let listener = std::net::TcpListener::bind("127.0.0.1:8001");
+    for _ in listener.unwrap().incoming(){
+        println!("New connection established");
+    }
+    Ok(())
 }
